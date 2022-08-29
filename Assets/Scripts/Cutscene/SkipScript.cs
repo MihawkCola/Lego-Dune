b72778191d02b0290a665ca95f71a18b6fc28b49
MihawkCola <32357495+MihawkCola@users.noreply.github.com>
2022-08-29 20:55:41 +0200

    cutsceene skip

--- Assets/Scripts/Cutscene/SkipScript.cs
+++ Assets/Scripts/Cutscene/SkipScript.cs
@@ -1,14 +1,24 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class ChangeLevel : MonoBehaviour {
-    // Changelevel on activation
+public class SkipScript : MonoBehaviour
+{
+    private SkipScene input;
+
+    private void Start()
+    {
+        Debug.Log("Sceene");
+        input = new SkipScene();
+        input.Enable();
+        input.skip.skip.started += loadLevel;
+    }
+
     private void loadLevel(InputAction.CallbackContext obj)
     {
+        Debug.Log("test");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
